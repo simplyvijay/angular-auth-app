6f08c83a3d5282c5bf25d96a069860301d1f3d11
Vijay Venkatasubramani <contact@simplyvijay.com>
2025-04-25 05:13:17 +0530

    Adding support for IMS configuration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
-import { Routes } from '@angular/router';
-import {Base64Component} from './base64/base64.component';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {Base64Component} from './base64/base64.component';
+import {ImsComponent} from './ims/ims.component'; // Import ImsComponent
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'base64', component: Base64Component }
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'base64', component: Base64Component},
+  {path: 'ims', component: ImsComponent} // Add route for ImsComponent
 ];
